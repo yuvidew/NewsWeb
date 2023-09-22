2b04f0549fd3d784f2f5d6cf4334b1765168ba76
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2023-09-23 02:59:57 +0530

    Update func.js

--- func.js
+++ func.js
@@ -8,20 +8,21 @@ async function thefethdata(quary){
     const response = await fetch(`https://newsapi.org/v2/everything?q=${quary}&apiKey=ee4f8b282ef9473a8c694fd21caa11fd`)
         const data = await response.json()
         let thaarrdata = data["articles"]
+        console.log(thaarrdata)
         let themaindata = thaarrdata.map(ele =>{
             return(
             `<div class = "thecardcont col-sm-12 col-md-6 col-lg-4 mt-5" 
-            <a href = ${ele.url}>
-                <div class="cardbox">
-                    <div class="imgbox">
-                        <img src=${ele.urlToImage} />
+                <a href = ${ele.url}>
+                    <div class="cardbox">
+                        <div class="imgbox">
+                            <img src=${ele.urlToImage} />
+                        </div>
+                        <div class="textbox">
+                            <h1>${ele.title}</h1>
+                            <p>${ele.description}</p>
+                        </div>
                     </div>
-                    <div class="textbox">
-                        <h1>${ele.title}</h1>
-                        <p>${ele.description}</p>
-                        </div>
-                        </div>
-            </a>
+                </a>
             </div>`)
         })
         thenewsdata.innerHTML = themaindata.join(" ")
